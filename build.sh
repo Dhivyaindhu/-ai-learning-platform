68377863c9f607aca9acd93034f7e43aa901ffc3
C.Indhupriya <117663766+Dhivyaindhu@users.noreply.github.com>
2026-03-17 12:34:32 +0530

    Integrate Tailscale setup in build script
    Added Tailscale installation and connection steps to the build process.

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Install Tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
+
+# Connect to Tailscale network
+if [ -n "$TAILSCALE_AUTH_KEY" ]; then
+  sudo tailscale up --authkey=$TAILSCALE_AUTH_KEY --accept-routes
+fi
+
+# Regular build steps
 pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
